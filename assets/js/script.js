09e756feea0fa24729bf040099f38d6047e52b6b
Heaven Sarder <sarder.heaven@gmail.com>
2023-12-18 16:38:27 +0600

    hero section

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// HEADER SLIDE CODE HERE
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    
+  });
