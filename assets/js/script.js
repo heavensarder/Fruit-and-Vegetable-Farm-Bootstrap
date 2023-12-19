8aae43ea661f97d64273d002a7443a3000d6446b
KawserAhmedRoni <kawserahmedroni51@gmail.com>
2023-12-19 18:40:41 +0600

    nex testimonial section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // HEADER SLIDE CODE HERE
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".heroSwiper", {
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
@@ -12,3 +12,21 @@ var swiper = new Swiper(".mySwiper", {
     },
     
   });
+
+//  TESTIMONIAL SLIDE CODE 
+var swiper = new Swiper(".testimonialSwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".next-button",
+    prevEl: ".previous-button",
+  },
+});
