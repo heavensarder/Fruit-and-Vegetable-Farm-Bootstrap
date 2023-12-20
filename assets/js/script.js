30101bbcb8013e03cbbf98d05d92a51c49370485
KawserAhmedRoni <kawserahmedroni51@gmail.com>
2023-12-20 12:51:40 +0600

    finished touch and new section

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,3 +30,36 @@ var swiper = new Swiper(".testimonialSwiper", {
     prevEl: ".previous-button",
   },
 });
+// SWIPER FOR LOGO SLIDE CODE STARTS HERE
+var swiper = new Swiper(".mySwiperLogo", {
+  slidesPerView: 5,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  // Default parameters
+  slidesPerView: 2,
+  spaceBetween: 20,
+
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40
+    }
+  }
+
+
+});
